3d40a81977c899632634005f8c2f6e514fbab6e4
Artem Zarovoski <artemzagorovski@mail.ru>
2018-10-14 21:13:15 +0300

    lab4

---
+++ Lab4/NuniTest/TriangleTests.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace MyClassTests
+
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void EqualSidesIsTrue()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(2, 2, 2);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void NegativeSideIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(-2, 2, 2);
+            Assert.AreEqual(expected, actual);
+        }
+      
+        [Test]
+        public void OneSideEqualSumOfTwoIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(2, 2, 4);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void UsualSidesIsTrue()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(3, 4, 5);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void OneSideBiggerThanSumOfTwoIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(2, 2, 6);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void OneSideDoesntExistIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(0, 2, 6);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void twoSidesAreEqualIsTrue()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(2, 6, 6);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TriangleDoesentExistIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(0, 0, 0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TwoSidesIsNegativeIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(-2, -2, 6);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TwoSidesDoesntExistIsFalse()
+        {
+            bool expected = true;
+
+            TriangleTest result = new TriangleTest();
+
+            int actual = result.Triangle(0, 0, 6);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
---
+++ Lab4/Triangle/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace UnitTests
+{
+    public static class TriangleTest
+    {
+        public static bool IsTriangle(double A, double B, double C)
+        {
+
+
+            if ((A < 0 || B < 0 || C < 0) && (B + C > A) || (A + C > B) || (A + B > C))
+                return false;
+
+            else return true;
+        }
+    }
+}
