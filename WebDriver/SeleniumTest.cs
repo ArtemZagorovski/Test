6ebb518b5616a87b5a09d9a41449f6947a0bb9ee
Artem Zarovoski <artemzagorovski@mail.ru>
2018-11-08 19:48:01 +0300

    Selenium

--- WebDriver/SeleniumTest.cs
+++ WebDriver/SeleniumTest.cs
@@ -6,21 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using Selenium;
-
-namespace Selenium
+namespace WebDriver
 {
-    public class Automatization
+    [TestFixture()]
+    public class Test
     {
-        public static void Main(string[] args){
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://www.flyuia.com/ua/ru/");
-            IWebElement FirstTown = driver.FindElement(By.Id("mat-input-0 "));
-            FirstTown.SendKeys("Минск");
-            IWebElement SecondTown = driver.FindElement(By.Id("mat-input-1 "));
-            SecondTown.SendKeys("Москва");
-            IWebElement Button = driver.FindElement(By.Id("SEARCH_WIDGET_FORM_BUTTONS_SEARCH_FLIGHTS"));
-            Button.Click();
+        [Test()]
+        public void SearchIsWorking()
+        {
+                IWebDriver driver = new ChromeDriver();
+                driver.Navigate().GoToUrl("https://www.flyuia.com/ua/ru/");
+                IWebElement FirstTown = driver.FindElement(By.Id("mat-input-0 "));
+                FirstTown.SendKeys("Минск");
+                IWebElement SecondTown = driver.FindElement(By.Id("mat-input-1 "));
+                SecondTown.SendKeys("Москва");
+                IWebElement Button = driver.FindElement(By.Id("SEARCH_WIDGET_FORM_BUTTONS_SEARCH_FLIGHTS"));
+                Button.Click();
+                IWebDriver CurUrl = new ChromeDriver();
+                String url = CurUrl.Url;
+                string expUrl = "https://book.flyuia.com/RU/default/flights?";
+                Assert.AreEqual(url, expUrl);
         }
     }
 }
