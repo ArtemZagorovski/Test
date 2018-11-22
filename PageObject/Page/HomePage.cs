e1a035371d8e6307d878bcc5ef81d0b76ba711e1
Artem Zagorovski <artemzagorovski@mail.ru>
2018-11-22 21:57:32 +0300

    lab 6

---
+++ PageObject/Page/HomePage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace AirlinesTestingApp.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+        private const string Url = "https://www.flyuia.com/by/ru/home";
+        By addPassenger = By.CssSelector(".ng-tns-c9-2 .value-as-text-container");
+        By plusBaby = By.CssSelector(".sw-row:nth-child(3) > .set-val-btn:nth-child(3)");
+        By errorsMessages = By.ClassName("messages");
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+        public void AddPassengers(){
+            driver.FindElement(addPassenger).Click();
+        }
+        public void PlusBaby()
+        {
+            driver.FindElement(plusBaby).Click();
+        }
+        public IWebElement GetErrorsMessages()
+        {
+            return driver.FindElement(errorsMessages);
+        }
+    }
+}
---
+++ PageObject/Test/POTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace WebDriver
+{
+    [TestFixture()]
+    public class Test
+    {
+        private HomePage homePage;
+        private const string ErrorMessage = "Каждый младенец может путещевствовать как минимум с одним взрослым";
+        [Test()]
+        public void OneBabyOnOneAdult()
+        {
+            OpenHomePage();
+            AddTwoBabys();
+            AssertErrorsVisible();
+        }
+        private void OpenHomePage()
+        {
+            var homePage = new HomePage(new ChromeDriver());
+            homePage.OpenHomePage();
+            homePage.CloseAds();
+            this.homePage = homePage;
+        }
+        private void AddTwoBabys()
+        {
+            homePage.AddPassengers();
+            homePage.PlusBaby();
+            homePage.PlusBaby();
+        }
+
+        private void AssertErrorsVisible()
+        {
+            var messageText = homePage.GetErrorsMessages().Text;
+            Assert.AreEqual(ErrorMessage, messageText);
+        }
+    }
+}
